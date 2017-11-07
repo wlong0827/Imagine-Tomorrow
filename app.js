ec7d910eef806028c60f0dfba8bad09540130672
Lucas Schuermann <lucas.v.schuermann@gmail.com>
2017-11-06 22:57:42 -0500

    basic working static files serving

--- app.js
+++ app.js
@@ -3,12 +3,8 @@
 const express = require('express');
 const app = express();
 
-// Use the built-in express middleware for serving static files from './public'
-app.use('/static', express.static('static'));
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// Use the built-in express middleware for serving static files from './frontend'
+app.use('/static', express.static('frontend'));
 
 // Start the server
 const PORT = process.env.PORT || 8080;
